8e3108dfe6426503d495833bb18d55b6d22bbd25
RaJ Patel <138092398+Raj2902Patel@users.noreply.github.com>
2024-03-19 23:10:39 +0530

    Add files via upload

---
+++ delete_record.php
@@ -0,0 +1,27 @@
+<?php
+
+include("dbconnection.php");
+
+    if(isset($_POST['name']))
+    {
+        $name = $_POST['name'];
+    }
+    else return;
+
+$query = "DELETE FROM `demo_database` WHERE name='$name'";
+
+$exe = mysqli_query($conn,$query);
+$arr = [];
+
+if($exe)
+{
+    $arr["Success"] = "true";
+}
+else
+{
+    $arr["success"] = "flase";
+}
+
+print(json_encode($arr));
+
+?>
